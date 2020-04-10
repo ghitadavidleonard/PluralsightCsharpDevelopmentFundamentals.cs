1bf39c63f4d31ac4991fb7f146f4c50c159d09ed
David Leonard <ghitadavidleonard@gmail.com>
2020-04-10 21:27:59 +0300

    Module 4 - Working with static members

--- CSharpFundamentals/CSharpFundamentals/Book.cs
+++ CSharpFundamentals/CSharpFundamentals/Book.cs
@@ -4,13 +4,23 @@ using System.Text;
 
 namespace CSharpFundamentals
 {
+    // Think of a class as a blueprint. It describes how I will build objects
     public class Book
     {
-        public Book()
+        // fields or properties (With fields you can't use implicit typing)
+        private List<double> grades;
+        private string name;
+
+        // constructors - the ideea of a constructor is that it constructs objects with some default values 
+        public Book(string name)
         {
-           
+            grades = new List<double>();
+            // this is a reference to the object that we operate on
+            this.name = name;
         }
 
+
+        // methods
         public double ComputeAverageGrade(List<double> grades)
         {
             var result = grades[0];
@@ -24,7 +34,7 @@ namespace CSharpFundamentals
 
         public void AddGrade(double grade)
         {
-     
+            grades.Add(grade);
         }
     }
 }
--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -8,24 +8,41 @@ namespace CSharpFundamentals
     //When your software is becoming too complex when you have a method that has to much code inside of it, that's when you should start thinking about "How can I use a new class to provide some encapsulation for the complexity I'm looking at"
     internal class Program
     {
+
+        // static members are not associated with an object instance. Static members are associated with the type that they are defined inside of
         private static void Main(string[] args)
         {
-            Book book = new Book();
-            book.AddGrade(42.4);
+            // example of static use
+            // Program.Main() <-- this is possible because the Main() method is associated with the type not with an object
 
+            Book book = new Book("David's Grade Book");
+            book.AddGrade(42.4);        // An object of type book. Because I have the class 'Book' I can create a books as long as I want and that book will have it's own behavior
+            book.AddGrade(90.5);
 
             var grades = new List<double>() { 12.3, 10.7, 6.11, 4.1, 45.3 };
             grades.Add(56.1);
 
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
 
             foreach (var grade in grades)
             {
+                if (grade > highGrade)
+                {
+                    highGrade = grade;
+                }
+
+                lowGrade = Math.Min(grade, lowGrade);
+
                 result += grade;
             }
 
             result /= grades.Count;
-            Console.WriteLine($"The average grade is {book.ComputeAverageGrade(grades):N1}");
+
+            Console.WriteLine($"The average grade is: {book.ComputeAverageGrade(grades):N1}");
+            Console.WriteLine($"The highest grade is: {highGrade}");
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
 
             if (args.Length > 0)
             {
