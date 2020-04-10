71c69a7a80c0884a556c0cc908ab723878ff97ce
David Leonard <ghitadavidleonard@gmail.com>
2020-04-10 07:51:11 +0300

    Module 3 - (Arrays, lists, looping, if conditions, statements)

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpFundamentals
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var grades = new List<double>() { 12.3, 10.7, 6.11, 4.1, 45.3 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[1]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
