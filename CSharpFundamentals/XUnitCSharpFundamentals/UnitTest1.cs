ae726b47643090fcefc4d42316d3637c943ab38f
David Leonard <ghitadavidleonard@gmail.com>
2020-04-10 22:54:52 +0300

    Writing and running a test

--- CSharpFundamentals/XUnitCSharpFundamentals/UnitTest1.cs
+++ CSharpFundamentals/XUnitCSharpFundamentals/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace XUnitCSharpFundamentals
 {
     public class UnitTest1
     {
+        // The test code should be executable statements
+        // We use "Fact" attribute for the actual unit tests methods
         [Fact]
         public void Test1()
         {
+            // arrange
+            var x = 5;
+            var y = 2;
+            var expected = 7;
 
+            // act
+            var actual = x + y;
+
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
