52ec001675492e521f8d5ba5777d26fc883b92e8
David Leonard <ghitadavidleonard@gmail.com>
2020-04-10 20:30:26 +0300

    Module 4 - Defining a method

---
+++ CSharpFundamentals/CSharpFundamentals/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamentals
+{
+    public class Book
+    {
+        public Book()
+        {
+           
+        }
+
+        public double ComputeAverageGrade(List<double> grades)
+        {
+            var result = grades[0];
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+
+            return result / grades.Count;
+        }
+
+        public void AddGrade(double grade)
+        {
+     
+        }
+    }
+}
--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace CSharpFundamentals
 {
+    //Module 4 - Working with Classes an Objects
+    //When your software is becoming too complex when you have a method that has to much code inside of it, that's when you should start thinking about "How can I use a new class to provide some encapsulation for the complexity I'm looking at"
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Book book = new Book();
+            book.AddGrade(42.4);
+
+
             var grades = new List<double>() { 12.3, 10.7, 6.11, 4.1, 45.3 };
             grades.Add(56.1);
 
@@ -18,7 +25,7 @@ namespace CSharpFundamentals
             }
 
             result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The average grade is {book.ComputeAverageGrade(grades):N1}");
 
             if (args.Length > 0)
             {
