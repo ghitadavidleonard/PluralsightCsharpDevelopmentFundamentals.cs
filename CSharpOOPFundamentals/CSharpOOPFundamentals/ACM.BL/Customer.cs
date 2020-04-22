6eb7627ba95b094f2e4f3ccfbd2fd4768cee2687
David Leonard <ghitadavidleonard@gmail.com>
2020-04-22 08:37:25 +0300

    Building a class, declaring props and using of snippets

---
+++ CSharpOOPFundamentals/CSharpOOPFundamentals/ACM.BL/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string FullName 
+        {
+            get => LastName + " " + FirstName;
+        }
+
+        private string _lastName;
+        public string LastName 
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+    }
+}
