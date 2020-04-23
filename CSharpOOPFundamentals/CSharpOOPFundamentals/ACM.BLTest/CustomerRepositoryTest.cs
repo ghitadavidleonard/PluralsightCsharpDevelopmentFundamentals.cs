31ee4373fc78c4831ce51696ad588fa8fc02da98
David Leonard <ghitadavidleonard@gmail.com>
2020-04-23 22:23:28 +0300

    Testing a composition relathionship

--- CSharpOOPFundamentals/CSharpOOPFundamentals/ACM.BLTest/CustomerRepositoryTest.cs
+++ CSharpOOPFundamentals/CSharpOOPFundamentals/ACM.BLTest/CustomerRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using ACM.BL;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ACM.BLTest
@@ -26,6 +27,61 @@ namespace ACM.BLTest
             Assert.Equal(expected.EmailAddress, actual.EmailAddress);
             Assert.Equal(expected.FirstName, actual.FirstName);
             Assert.Equal(expected.LastName, actual.LastName);
+        }
+
+        [Fact]
+        public void RetrieveExistingWithAddress()
+        {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                AddressList = new List<Address>()
+                {
+                    new Address()
+                    {
+                        AddressType = 1,
+                        StreetLineOne = "Bag End",
+                        StreetLineTwo = "Bagshot row",
+                        City = "Hobbiton",
+                        State = "Shire",
+                        Country = "Middle Earth",
+                        PostalCode = "144"
+                    },
+
+                    new Address()
+                    {
+                        AddressType = 2,
+                        StreetLineOne = "Green Dragon",
+                        City = "Bywater",
+                        State = "Shire",
+                        Country = "Middle Earth",
+                        PostalCode = "146"
+                    }
+                }
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.Equal(expected.CustomerId, actual.CustomerId);
+            Assert.Equal(expected.EmailAddress, actual.EmailAddress);
+            Assert.Equal(expected.FirstName, actual.FirstName);
+            Assert.Equal(expected.LastName, actual.LastName);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.Equal(expected.AddressList[i].AddressType, actual.AddressList[i].AddressType);
+                Assert.Equal(expected.AddressList[i].StreetLineOne, actual.AddressList[i].StreetLineOne);
+                Assert.Equal(expected.AddressList[i].City, actual.AddressList[i].City);
+                Assert.Equal(expected.AddressList[i].State, actual.AddressList[i].State);
+                Assert.Equal(expected.AddressList[i].Country, actual.AddressList[i].Country);
+                Assert.Equal(expected.AddressList[i].PostalCode, actual.AddressList[i].PostalCode);
+            }
         }
     }
 }
