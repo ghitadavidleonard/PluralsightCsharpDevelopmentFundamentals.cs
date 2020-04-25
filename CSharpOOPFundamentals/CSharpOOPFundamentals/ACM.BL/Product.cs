3cb04d62fb7c79c328fe8da51294358aa366024b
David Leonard <ghitadavidleonard@gmail.com>
2020-04-25 18:00:21 +0300

    Using the reusable component

--- CSharpOOPFundamentals/CSharpOOPFundamentals/ACM.BL/Product.cs
+++ CSharpOOPFundamentals/CSharpOOPFundamentals/ACM.BL/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acme.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,7 +13,8 @@ namespace ACM.BL
         {
             get 
             {
-                return _productName;
+                var handler = new StringHandler();
+                return handler.InsertSpaces(_productName);
             }
             set 
             {
