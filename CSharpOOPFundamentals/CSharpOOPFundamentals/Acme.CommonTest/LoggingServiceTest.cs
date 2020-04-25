759ae72ed3046c58c0c82f42f47d824105b2c813
David Leonard <ghitadavidleonard@gmail.com>
2020-04-25 19:59:33 +0300

    Interface based pollymorphism

---
+++ CSharpOOPFundamentals/CSharpOOPFundamentals/Acme.CommonTest/LoggingServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using ACM.BL;
+using Acme.Common;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Acme.CommonTest
+{
+    public class LoggingServiceTest
+    {
+        [Fact]
+        public void WriteToFileTest()
+        {
+            //-- Arrange
+            var changedItems = new List<ILoggable>();
+
+            var customer = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                AddressList = null
+            };
+            changedItems.Add(customer);
+
+            var product = new Product(1)
+            {
+                ProductName = "Rake",
+                Description = "Garden Rake with Steel Head",
+                CurrentPrice = 6M
+            };
+            changedItems.Add(product);
+
+            //-- Act
+            LoggingService.WriteToFile(changedItems);
+
+            //-- Assert
+            // Nothing to assert
+        }
+    }
+}
